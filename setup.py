fe14baaad53be7140f07f8cc663135ecda5e4c5c
Geoff Greer <geoff@greer.fm>
2013-06-07 12:31:17 -0700

    Fine. Remove this line so the build works.

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-import os
-
 from setuptools import setup
 
 setup(name='Flootty',
-      version='0.11',
+      version='0.12',
       description='Floobits collaborative terminal',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
       author='Floobits',
       author_email='info@floobits.com',
       url='https://floobits.com/',
