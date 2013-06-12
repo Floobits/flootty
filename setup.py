4b00557823e47aa9d59b473c031d03ba3868316f
Geoff Greer <geoff@greer.fm>
2013-06-12 10:44:53 -0700

    New release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Flootty',
-      version='0.13',
+      version='0.14',
       description='Floobits collaborative terminal',
       author='Floobits',
       author_email='info@floobits.com',
