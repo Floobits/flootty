069d3d0cd4d5ba6624097145dfe545b70f867cb7
Geoff Greer <geoff@greer.fm>
2013-06-26 12:54:52 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Flootty',
-      version='0.14',
+      version='0.15',
       description='Floobits collaborative terminal',
       author='Floobits',
       author_email='info@floobits.com',
