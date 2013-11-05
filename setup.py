c30f0e59be5cc7ebd21124d8343e08465a4f893e
Matt Kaniaris <mkaniaris@gmail.com>
2013-11-04 16:47:51 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Flootty',
-      version='1.01',
+      version='1.02',
       description='Floobits collaborative terminal',
       author='Floobits',
       author_email='info@floobits.com',
