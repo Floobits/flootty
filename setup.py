0e4672cc153d32a5810069fc9e9582d382bf9092
Geoff Greer <geoff@greer.fm>
2014-02-21 19:47:19 -0800

    Use semver.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Flootty',
-      version='1.10',
+      version='2.0.0',
       description='Floobits collaborative terminal',
       author='Floobits',
       author_email='info@floobits.com',
